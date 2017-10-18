ba2ad2620c15845ce462f34ea4de9c36ba43fc04
khotz <kai.hotz@complevo.de>
2017-10-18 12:04:30 +0200

    Added function to handle error messages in utils folder

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,3 +8,18 @@ export function createActionTypes (base, actions = []) {
 export function createAction (type, data = {}) {
   return {type, payload: data}
 }
+
+export const errorMessage = (error, format) => {
+  if (!error) {
+    return ''
+  }
+
+  let code = error.errorCode
+  if (!code) {
+    code = error.response && error.response.status === 401
+      ? 'Unauthorized'
+      : 'InternalServerError'
+  }
+
+  return format({id: `error.${code}`})
+}
