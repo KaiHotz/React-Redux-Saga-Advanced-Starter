57314ce87e4e5863446f0fce3780101c53a029a7
Kai Hotz <kaihotz@me.com>
2020-08-23 08:46:35 +0400

    update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
-import { ConnectedRouter } from 'connected-react-router'
 import { createBrowserHistory } from 'history'
 
 import * as serviceWorker from './serviceWorker'
